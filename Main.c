74b7594a7cd1376fb3d8a7591afcec379a7bfc76
Oscar Araujo <a20844@alunos.ipca.pt>
2020-10-30 10:43:32 +0000

    Fase Inicial

---
+++ Main.c
@@ -0,0 +1,13 @@
+/**
+* Autor:Oscar Araújo
+* Nº Aluno: 20844
+* Email: a20844@alunos.ipca.pt
+* Disciplina: Programação Imperativa
+**/
+
+#include <stdio.h>								///Introdução da biblioteca
+
+int main()										///Introdução da função main
+{
+	printf ("OLA, O meu nome e Oscar.");		///mandar mostrar algo na tela
+}
